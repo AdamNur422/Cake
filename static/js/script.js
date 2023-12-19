675c44958344bfa1183c86e443f22d574af413d4
AdamNur422 <143763008+AdamNur422@users.noreply.github.com>
2023-12-19 00:01:17 -0500

    Cake 3

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,12 @@ let age = 0;
 let candlePositions = [];
 const OFFSET = 165;
 const OVERLAP_THRESHOLD = 1;
+const placeCandleSound = new Audio('/static/sounds/place_candle.mp3');
+const removeCandleSound = new Audio('/static/sounds/remove_candle.mp3');
+const volumeControl = document.getElementById('volume');
+const volumeIcon = document.getElementById('volume-icon');
 
+// Adding Candles
 function placeCandle(event) {
     const candleOffset = 10; 
     const cakeContainer = document.getElementById('cake-container');
@@ -27,6 +32,10 @@ function placeCandle(event) {
     newCandle.style.top = '-60px';
     cakeContainer.appendChild(newCandle);
 
+    placeCandleSound.playbackRate = 1.5;
+    placeCandleSound.play();
+
+
     setTimeout(() => {
         newCandle.style.top = (yPosition - newCandle.offsetHeight + OFFSET) + 'px';
     }, 100);
@@ -43,6 +52,7 @@ function placeCandle(event) {
     console.log('placeCandle called');
 }
 
+// Checking Overlapping 
 function isOverlapping(position) {
     return candlePositions.some(existingPosition => {
         const dx = existingPosition.x - position.x;
@@ -51,7 +61,11 @@ function isOverlapping(position) {
     });
 }
 
+// Remove Candle
 function removeCandle(event, candle) {
+    removeCandleSound.playbackRate = 2.5; 
+    removeCandleSound.play();
+
     event.stopPropagation(); 
     candle.remove(); 
 
@@ -71,6 +85,51 @@ function removeCandle(event, candle) {
     console.log('removeCandle called');
 }
 
+// Open and close the settings panel
+document.getElementById('settings-button').addEventListener('click', function() {
+    document.getElementById('settings-panel').style.display = 'block';
+    document.body.classList.add('modal-open'); 
+});
+
+document.querySelector('.close-button').addEventListener('click', function() {
+    document.getElementById('settings-panel').style.display = 'none';
+    document.body.classList.remove('modal-open'); 
+});
+
+// Volume control
+volumeControl.addEventListener('input', function(e) {
+    const volume = e.target.value;
+
+    const maxVolume = volumeControl.max || 1; 
+    const filledPercentage = (volume / maxVolume) * 100;
+    volumeControl.style.setProperty('--filled', filledPercentage + '%');
+
+    placeCandleSound.volume = volume;
+    removeCandleSound.volume = volume;
+    updateVolumeIcon(volume);
+});
+
+volumeIcon.addEventListener('click', function() {
+    if (volumeControl.value !== "0") {
+        volumeControl.value = 0;
+        volumeControl.style.setProperty('--filled', '0%'); 
+        updateVolumeIcon(0);
+    } else {
+        volumeControl.value = 1;
+        volumeControl.style.setProperty('--filled', '100%'); 
+        updateVolumeIcon(1);
+    }
+});
+
+function updateVolumeIcon(volume) {
+    if (volume == 0) {
+        volumeIcon.className = 'fas fa-volume-mute'; 
+    } else if (volume <= 0.5) {
+        volumeIcon.className = 'fas fa-volume-down'; 
+    } else {
+        volumeIcon.className = 'fas fa-volume-up'; 
+    }
+}
 
 
 document.getElementById('cake-container').addEventListener('click', placeCandle);
